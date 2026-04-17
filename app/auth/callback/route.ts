c6db4c5b714095e1a7499c5a0e29fb046e568d86
z5-art <omarkibbous@gmail.com>
2026-04-18 00:42:18 +0100

    fix empty route files

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,19 @@
+import { createClient } from '@supabase/supabase-js'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url)
+  const code = searchParams.get('code')
+
+  if (code) {
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+    await supabase.auth.exchangeCodeForSession(code)
+  }
+
+  return NextResponse.redirect(
+    new URL('/', req.url)
+  )
+}
